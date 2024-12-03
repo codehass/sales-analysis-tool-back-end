c3b231158fc303df6e6c7d574668466e073c5114
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2024-12-03 09:43:40 +0100

    create routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,32 @@ const app = express();
 
 const PORT = 3000;
 
+//routes
+
+//analytics/total_sales 
+app.get('/api/v1/analytics/total_sales', (req:Request, res:Response, next:NextFunction ) => {
+  res.send('Total Sales');
+} );
+
+
+//analytics/trending_products
+app.get('/api/v1/analytics/trending_products', (req:Request, res:Response, next:NextFunction ) => {
+  res.send('Trending Products');
+} ); 
+
+
+//analytics/category_sales
+app.get('/api/v1/analytics/category_sales', (req:Request, res:Response, next:NextFunction ) => {
+  res.send('Category Sales');
+} );
+
+
+//products
+app.get('/api/v1/products', (req:Request, res:Response, next:NextFunction ) => {
+  res.send('Products');
+} ); 
+
+
 app.get('/api/v1/hello', (req:Request, res:Response, next:NextFunction ) => {
   res.send('Hello World!');
 } );
